5c39462ddb85a2327194a35884abb2cbaa2d86ff
maks4a <maks4a@gmail.com>
2019-05-13 22:22:08 +0300

    Поиск всех входящих значений

--- ParsingTools.pas
+++ ParsingTools.pas
@@ -3,12 +3,14 @@ unit ParsingTools;
 interface
 
 uses
+  System.Generics.Collections,
   System.SysUtils;
 
 type
   IParsTools = interface
     ['{ACC6F3C9-2C01-42C7-A08D-36B93C69F6E5}']
     function First(const AFrom, ATo: string): string;
+    function All(const AFrom, ATo: string): TArray<string>;
   end;
 
   TParsTools = class(TInterfacedObject, IParsTools)
@@ -17,11 +19,30 @@ type
   public
     class function Extract(const AText: string): IParsTools;
     function First(const AFrom, ATo: string): string;
+    function All(const AFrom, ATo: string): TArray<string>;
   end;
 
 implementation
 
 { TParsTools }
+
+function TParsTools.All(const AFrom, ATo: string): TArray<string>;
+var
+  LFinded: TList<string>;
+  LCurrent: string;
+begin
+  LFinded := TList<string>.Create;
+  try
+    while not FText.IsEmpty do
+    begin
+      LCurrent := First(AFrom, ATo);
+      LFinded.Add(LCurrent);
+    end;
+    Result := LFinded.ToArray;
+  finally
+    LFinded.Free;
+  end;
+end;
 
 class function TParsTools.Extract(const AText: string): IParsTools;
 begin
@@ -38,6 +59,10 @@ begin
   FText := FText.Remove(0, LFirst);
   LLast := FText.IndexOf(ATo);
   Result := FText.Substring(0, LLast);
+  if (LFirst < 0) or (LLast < 0) then
+    FText := ''
+  else
+    FText := FText.Substring(LLast);
 end;
 
 end.
--- Tests/Test.ParsingTools.pas
+++ Tests/Test.ParsingTools.pas
@@ -23,10 +23,16 @@ type
     [TestCase('TestA',
       '<meta name="csrf-token" content="xJC904V7lCCUbrjWYxqCj_xecjEbcMOSuwj1JuHHcrKm0fbktTXEauEm2pwNSMu8iTYTBmg6ucDUPKpXrLYr3Q==">,<meta name="csrf-token" content=",">,xJC904V7lCCUbrjWYxqCj_xecjEbcMOSuwj1JuHHcrKm0fbktTXEauEm2pwNSMu8iTYTBmg6ucDUPKpXrLYr3Q==')]
     [TestCase('TestB', '<b>data</b>,<b>,</b>,data')]
-    procedure Test2(const AValue, AFrom, ATo, AData: string);
+    procedure First(const AValue, AFrom, ATo, AData: string);
+    [Test]
+    [TestCase('TestA', '<b>data</b><b>data2</b><b>data3</b>,<b>,</b>,datadata2data3')]
+    procedure All(const AValue, AFrom, ATo, AData: string);
   end;
 
 implementation
+
+uses
+  System.SysUtils;
 
 procedure TMyTestObject.Setup;
 begin
@@ -37,7 +43,17 @@ procedure TMyTestObject.TearDown;
 begin
 end;
 
-procedure TMyTestObject.Test2(const AValue, AFrom, ATo, AData: string);
+procedure TMyTestObject.All(const AValue, AFrom, ATo, AData: string);
+var
+  LActualRaw: TArray<string>;
+  LActual: string;
+begin
+  LActualRaw := TParsTools.Extract(AValue).All(AFrom, ATo);
+  LActual := string.Join('', LActualRaw);
+  Assert.AreEqual(AData, LActual);
+end;
+
+procedure TMyTestObject.First(const AValue, AFrom, ATo, AData: string);
 var
   LActual: string;
 begin
